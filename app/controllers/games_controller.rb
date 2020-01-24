b6ab5f76dca4c4aaa60d453b0344827d04dc3fd7
Aline Gasparindo <aline.gasparindo@gmail.com>
2020-01-23 20:01:51 -0500

    checking if the word exists : score page done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,57 +1,27 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
+
   def new
     key = ""
     10.times { key += ('a'..'z').to_a.sample.to_s }
-    @grid = key
+    @letters = key
   end
 
   def score
+    response = open("https://wagon-dictionary.herokuapp.com/#{params[:word]}")
+    json = JSON.parse(response.read)
+    result = ""
+    if params[:word].chars.all? { |letter| params[:word].count(letter) <= params[:letter_list].count(letter) }
+      if json['found']
+        @result = "well done"
+      else
+        @result = "not an english word"
+      end
+    else
+      @result = "not in the grid"
+    end
   end
 end
 
-
-# require 'open-uri'
-# require 'json'
-
-# def generate_grid(grid_size)
-#   key = ""
-#   grid_size.times { key += ('a'..'z').to_a.sample.to_s }
-#   return key
-#   # TODO: generate random grid of letters
-# end
-
-# def validword?(attempt)
-#   url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
-#   dic = open(url).read
-#   something = JSON.parse(dic)
-#   return something["found"]
-# end
-
-# def in_the_grid(attempt, grid)
-#   if attempt.upcase.split('').all? { |i| grid.include? i }
-#     return true
-#   else
-#     return false
-#   end
-# end
-
-
-# def run_game(attempt, grid, start_time, end_time)
-
-#   nh = {
-#     time: end_time - start_time,
-#     score: 0,
-#     message: ""
-#   }
-
-#   if in_the_grid(attempt, grid) == false
-#     nh[:message] = "Not in the grid."
-#   elsif validword?(attempt) == false
-#     nh[:message] = "not an english word"
-#   elsif validword?(attempt) && in_the_grid(attempt, grid)
-#     nh[:message] = "Well done!"
-#     nh[:score] =+20 + attempt.length - nh[:time]
-#   end
-
-#   return nh
-# end
